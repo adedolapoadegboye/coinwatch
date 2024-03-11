6b3bd5a0f510d094e9af5d8e8642e38af78045da
Adedolapo Adegboye <adedolapo.adegboye@alumni.ucalgary.ca>
2024-03-11 00:31:11 -0600

    Add income enry completed

--- coinwatch/src/Components/DashboardPage/Components/Add.jsx
+++ coinwatch/src/Components/DashboardPage/Components/Add.jsx
@@ -7,12 +7,36 @@ import EntryTypeSubscriptions from "../Subcomponents/EntryTypeSubscriptions";
 import EntryTypeInvestments from "../Subcomponents/EntryTypeInvestments";
 
 const Add = ({ setAdd, add }) => {
+  const [selectedValue, setSelectedValue] = useState("Income");
+  const [dataFromIncomeForm, setDataFromIncomeForm] = useState(null);
+  const [handleErrorMessage, setHandleErrorMessage] = useState(false);
+
   // Function to handle closing the Add component
   const handleClose = () => {
     setAdd(false);
   };
 
-  const [selectedValue, setSelectedValue] = useState("Income");
+  const handleError = () => {
+    if (handleErrorMessage) {
+      return (
+        <h1 className="noto-sans-1 text-red-400">
+          Error! Please enter a valid amount.
+        </h1>
+      );
+    } else {
+    }
+  };
+
+  // Function to handle submitting and closing the Add component
+  const handleSubmit = () => {
+    console.log(dataFromIncomeForm);
+    if (dataFromIncomeForm.amount <= 0) {
+      setHandleErrorMessage(true);
+    } else {
+      setHandleErrorMessage(false);
+      setAdd(false);
+    }
+  };
 
   const handleSelect = (value) => {
     setSelectedValue(value);
@@ -28,7 +52,7 @@ const Add = ({ setAdd, add }) => {
 
   const renderInputForm = () => {
     if (selectedValue === "Income") {
-      return <EntryTypeIncome />;
+      return <EntryTypeIncome handleIncomeForm={handleIncomeForm} />;
     } else if (selectedValue === "Expenses") {
       return <EntryTypeExpenses />;
     } else if (selectedValue === "Investments") {
@@ -40,9 +64,13 @@ const Add = ({ setAdd, add }) => {
     }
   };
 
+  const handleIncomeForm = (incomeDataForm) => {
+    setDataFromIncomeForm(incomeDataForm);
+  };
+
   return (
     <div className="fixed inset-0 z-10 flex justify-center items-center bg-black bg-opacity-50 w-full h-full">
-      <div className="bg-white rounded-lg p-8 flex flex-col h-fit w-[calc(100vw/1.5)] lg:w-[calc(100vw/2)] gap-4 lg:gap-8">
+      <div className="bg-white rounded-lg p-8 flex flex-col h-fit w-[calc(100vw/1.2)] lg:w-[calc(100vw/2)] gap-2 lg:gap-4">
         <h2 className="text-xl font-bold mb-4">Add Entry</h2>
         <div className="flex flex-col justify-center lg:items-center h-fit lg:flex-row gap-2 w-full">
           <p className="text-gray-600 text-sm w-1/3 text-nowrap">
@@ -55,7 +83,7 @@ const Add = ({ setAdd, add }) => {
         <div>{renderInputForm()}</div>
         <div className="flex gap-4 lg:gap-10">
           <button
-            onClick={handleClose}
+            onClick={handleSubmit}
             className="active-white-timerange rounded-sm border-gray-300 w-full inter-heading-2 border-2 md:border-2 text-sm font-light text-black capitalize leading-snug tracking-tight mb-2 sm:mb-0 px-2 py-3"
           >
             Submit
@@ -67,6 +95,7 @@ const Add = ({ setAdd, add }) => {
             Close
           </button>
         </div>
+        {handleError()}
       </div>
     </div>
   );
--- coinwatch/src/Components/DashboardPage/Subcomponents/EntryTypeIncome.jsx
+++ coinwatch/src/Components/DashboardPage/Subcomponents/EntryTypeIncome.jsx
@@ -1,15 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import EntryType from "./EntryType";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-const EntryTypeIncome = () => {
+const EntryTypeIncome = ({ handleIncomeForm }) => {
+  const firstDayOfMonth = new Date();
+  firstDayOfMonth.setDate(1); // Set the date to the first day of the month
   const [formData, setFormData] = useState({
-    name: "",
+    date: firstDayOfMonth, // Set default value for date field
     amount: "",
-    category: "",
+    category: "Salary/Wage", // Set default value for category field
     notes: "",
   });
 
-  const [dumb, setDumb] = useState(false);
+  useEffect(() => {
+    // Trigger the function to handle income form data whenever formData changes
+    console.log(formData);
+    handleIncomeForm(formData);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [formData]);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -17,7 +26,7 @@ const EntryTypeIncome = () => {
       ...formData,
       [name]: value,
     });
-    dumbRender();
+    // dumbRender();
   };
 
   const handleCategoryInputChange = (value) => {
@@ -25,14 +34,8 @@ const EntryTypeIncome = () => {
       ...formData,
       category: value,
     });
-    dumbRender();
+    // dumbRender();
   };
-
-  const dumbRender = () => {
-    setDumb(!dumb);
-  };
-
-  console.log(formData);
 
   const options = [
     "Salary/Wage",
@@ -45,24 +48,72 @@ const EntryTypeIncome = () => {
     "Others",
   ];
 
+  //   const [endDate, setEndDate] = useState(new Date());
+  const [dateButton, setDateButton] = useState(false);
+  const [startDate, setStartDate] = useState(firstDayOfMonth);
+
+  // Calculate the first day of the current month
+
+  // Function to handle date selection
+  const handleDateChange = (date) => {
+    // const [start, end] = dates;
+    setStartDate(date);
+    // setEndDate(end);
+    setFormData({
+      ...formData,
+      date: date,
+    });
+    // dumbRender();
+  };
+
+  // Function to toggle date picker visibility
+  const toggleDatePicker = () => {
+    setDateButton(!dateButton);
+  };
+
+  // Function to format date in "MMMM d, yyyy" format
+  const formatDate = (date) => {
+    // Check if date is null or undefined
+    if (!date) return "";
+
+    // Options for date formatting
+    const options = { month: "long", day: "numeric", year: "numeric" };
+
+    // Format the date using toLocaleDateString() method
+    return new Date(date).toLocaleDateString("en-US", options);
+  };
+
   return (
-    <div className="flex flex-col gap-8 w-full h-full">
+    <div className="flex flex-col gap-4 w-full h-full">
       <div className="flex flex-col justify-center lg:items-center h-fit lg:flex-row gap-2 w-full">
         <label
           htmlFor="name"
           className="text-gray-600 text-sm w-1/3 text-nowrap"
         >
-          Label
+          Date Received
         </label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          required
-          value={formData.name}
-          onChange={handleInputChange}
-          className="inline-flex justify-between w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
-        />
+        <div className="w-full flex flex-col gap-2 relative">
+          <button
+            className="inline-flex justify-between w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+            onClick={toggleDatePicker}
+          >
+            {`${formatDate(startDate)}`}
+          </button>
+          {dateButton && (
+            <div className="flex justify-center gap-2 z-20">
+              <DatePicker
+                showIcon
+                selected={startDate}
+                onChange={handleDateChange}
+                startDate={startDate}
+                // endDate={endDate}
+                inline
+                // minDate={new Date()} // Disable past dates
+                maxDate={new Date()} // Set maximum date to one week from start date
+              />
+            </div>
+          )}
+        </div>
       </div>
       <div className="flex flex-col justify-center lg:items-center h-fit lg:flex-row gap-2 w-full">
         <label
